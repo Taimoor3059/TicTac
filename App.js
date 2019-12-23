85849ed52f8cd0a0b7244d9a57b46caf66af3077
TaimoorTahir <taimoorhtahir@gmail.com>
2019-12-23 12:55:03 -0600

    Update App.js

--- App.js
+++ App.js
@@ -48,21 +48,21 @@ export default class App extends React.Component {
 
   winGame = () => {
     if ( (itemArray[0] !== "empty") && (itemArray[0] == itemArray[1]) && (itemArray[1] == itemArray[2]) ) {
-      this.setState({winMessaged: (itemArray[0] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[0] ? "cross" : "circle").concat(" Win") })
     } else if ( (itemArray[3] !== "empty") && (itemArray[3] == itemArray[4]) && (itemArray[4] == itemArray[5]) ) {
-      this.setState({winMessaged: (itemArray[3] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[3] ? "cross" : "circle").concat(" Win") })
     } else if ( (itemArray[6] !== "empty") && (itemArray[6] == itemArray[7]) && (itemArray[7] == itemArray[8]) ) {
-      this.setState({winMessaged: (itemArray[6] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[6] ? "cross" : "circle").concat(" Win") })
     } else if ( (itemArray[0] !== "empty") && (itemArray[0] == itemArray[3]) && (itemArray[3] == itemArray[6]) ) {
-      this.setState({winMessaged: (itemArray[0] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[0] ? "cross" : "circle").concat(" Win") })
     } else if ( (itemArray[1] !== "empty") && (itemArray[1] == itemArray[4]) && (itemArray[4] == itemArray[7]) ) {
-      this.setState({winMessaged: (itemArray[1] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[1] ? "cross" : "circle").concat(" Win") })
     } else if ( (itemArray[2] !== "empty") && (itemArray[2] == itemArray[5]) && (itemArray[5] == itemArray[8]) ) {
-      this.setState({winMessaged: (itemArray[2] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[2] ? "cross" : "circle").concat(" Win") })
     } else if ( (itemArray[0] !== "empty") && (itemArray[0] == itemArray[4]) && (itemArray[4] == itemArray[8]) ) {
-      this.setState({winMessaged: (itemArray[0] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[0] ? "cross" : "circle").concat(" Win") })
     } else if ( (itemArray[2] !== "empty") && (itemArray[2] == itemArray[4]) && (itemArray[4] == itemArray[8]) ) {
-      this.setState({winMessaged: (itemArray[2] ? "Cross" : "Circle").concat(" Win") })
+      this.setState({winMessaged: (itemArray[2] ? "cross" : "circle").concat(" Win") })
     }
   }
 
